9e44ca282f83e78743c1b2f818b408d62fb94276
Leonid Beder <leonid@jammer.fm>
2015-04-02 18:19:30 +0300

    Merged.

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -1,4 +1,9 @@
 node[:deploy].each do |application, deploy|
+
+  unless node[:sidekiq][application]
+    next
+  end
+
   Chef::Log.debug("Restarting Sidekiq Application: #{application}")
   execute "restart Rails app #{application}" do
     command node[:sidekiq][application][:restart_command]
